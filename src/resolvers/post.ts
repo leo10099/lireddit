ab40b90e2d32452e4b21799285fcb1284f93ddfa
leo10099 <leandrohanc@gmail.com>
2020-08-22 14:37:14 -0300

    Add class-validator. Complete CRUD ops for Post

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Ctx } from "type-graphql";
+import { Resolver, Mutation, Query, Ctx, Arg } from "type-graphql";
 import { Post } from "../entities/Post";
 
 // Typings
@@ -7,7 +7,42 @@ import { AppContext } from "../typings";
 @Resolver()
 export class PostResolver {
 	@Query(() => [Post])
-	posts(@Ctx() ctx: AppContext): Promise<Post[]> {
-		return ctx.db.find(Post, {});
+	posts(@Ctx() { db }: AppContext): Promise<Post[]> {
+		return db.find(Post, {});
+	}
+
+	@Query(() => Post, { nullable: true })
+	post(@Arg("id") id: number, @Ctx() { db }: AppContext): Promise<Post | null> {
+		return db.findOne(Post, { id });
+	}
+
+	@Mutation(() => Post)
+	async createPost(@Arg("title") title: string, @Ctx() { db }: AppContext): Promise<Post | null> {
+		const post = db.create(Post, { title });
+		await db.persistAndFlush(post);
+
+		return post;
+	}
+
+	@Mutation(() => Post, { nullable: true })
+	async updatePost(
+		@Arg("id") id: number,
+		@Arg("title", () => String, { nullable: true }) title: string,
+		@Ctx() { db }: AppContext,
+	): Promise<Post | null> {
+		const post = await db.findOne(Post, { id });
+
+		if (!post || !title) return null;
+
+		post.title = title;
+		await db.persistAndFlush(post);
+
+		return post;
+	}
+
+	@Mutation(() => Boolean)
+	async deletePost(@Arg("id") id: number, @Ctx() { db }: AppContext): Promise<boolean> {
+		await db.nativeDelete(Post, { id });
+		return true;
 	}
 }
