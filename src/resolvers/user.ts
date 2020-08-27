57501d162aa6e14d1e670a98f31ec1c4a867f13d
leo10099 <leandrohanc@gmail.com>
2020-08-27 20:40:17 -0300

    Add login mutation

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,4 +1,4 @@
-import { Resolver, Mutation, Query, Ctx, Arg, InputType, Field } from "type-graphql";
+import { Resolver, Mutation, Ctx, Arg, InputType, Field, ObjectType } from "type-graphql";
 import { User } from "../entities/User";
 import * as argon2 from "argon2";
 
@@ -6,27 +6,87 @@ import * as argon2 from "argon2";
 import { AppContext } from "../typings";
 
 @InputType()
-class RegisterInput {
+class AuthInput {
 	@Field()
 	username: string;
 	@Field()
 	password: string;
 }
 
+@ObjectType()
+class FieldError {
+	@Field()
+	field: string;
+	@Field()
+	message: string;
+}
+
+@ObjectType()
+class UserResponse {
+	@Field(() => [FieldError], { nullable: true })
+	errors?: FieldError[];
+
+	@Field(() => User, { nullable: true })
+	user?: User;
+}
+
 @Resolver()
 export class UserResolver {
-	@Mutation(() => User)
+	@Mutation(() => UserResponse)
 	async register(
-		@Arg("registerInput") registerInput: RegisterInput,
+		@Arg("registerInput") registerInput: AuthInput,
 		@Ctx() { db }: AppContext,
-	): Promise<User> {
+	): Promise<UserResponse> {
+		if (registerInput.username.length <= 2) {
+			return {
+				errors: [{ field: "username", message: "Username must have at least 3 characters" }],
+			};
+		}
+		if (registerInput.password.length <= 2) {
+			return {
+				errors: [{ field: "password", message: "Password must have at least 3 characters" }],
+			};
+		}
+
 		const hashedPassword = await argon2.hash(registerInput.password);
 		const user = db.create(User, {
 			username: registerInput.username,
 			password: hashedPassword,
 		});
-		await db.persistAndFlush(user);
 
-		return user;
+		try {
+			await db.persistAndFlush(user);
+		} catch (e) {
+			// duplicate username
+			if (e.code === "23505") {
+				return {
+					errors: [{ field: "username", message: "Username already taken" }],
+				};
+			}
+		}
+
+		return { user };
+	}
+
+	@Mutation(() => UserResponse)
+	async login(
+		@Arg("loginInput") loginInput: AuthInput,
+		@Ctx() { db }: AppContext,
+	): Promise<UserResponse> {
+		const user = await db.findOne(User, { username: loginInput.username });
+		if (!user) {
+			return {
+				errors: [{ field: "username", message: "Invalid credentials" }],
+			};
+		}
+		const isPasswordValid = await argon2.verify(user.password, loginInput.password);
+
+		if (!isPasswordValid) {
+			return {
+				errors: [{ field: "password", message: "Invalid credentials" }],
+			};
+		}
+
+		return { user };
 	}
 }
